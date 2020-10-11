86a56f2867bd35b022700b5ef0ec1edfe0de8462
Zachery <zachery@foxboys.online>
2020-10-11 16:24:16 -0500

    Fix walking speed and bump version

--- src/main/java/com/dumbdogdiner/stickycommands/commands/Speed.java
+++ src/main/java/com/dumbdogdiner/stickycommands/commands/Speed.java
@@ -1,6 +1,5 @@
 package com.dumbdogdiner.stickycommands.commands;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.TreeMap;
@@ -11,7 +10,6 @@ import com.dumbdogdiner.stickycommands.User;
 import com.ristexsoftware.koffee.arguments.Arguments;
 import com.ristexsoftware.koffee.bukkit.command.AsyncCommand;
 import com.ristexsoftware.koffee.translation.LocaleProvider;
-import com.ristexsoftware.koffee.util.NumberUtil;
 
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -35,7 +33,6 @@ public class Speed extends AsyncCommand {
         User user = Main.getInstance().getOnlineUser(((Player)sender).getUniqueId());
         Arguments a = new Arguments(args);
         a.requiredString("speed");
-        // a.optionalString("type");
 
         if (!a.valid())
             return 1;
@@ -46,21 +43,7 @@ public class Speed extends AsyncCommand {
         var speed = Float.parseFloat(a.get("speed")) / 10;
         if (speed*10> 10 || speed*10 <= 0)
             return 1;
-        
-        // if (a.exists("type")) {
-        //     switch(a.get("type").toLowerCase()) {
-        //         case "w":
-        //         case "walk":
-        //         case "walking":
-        //             setSpeed(user, SpeedType.WALK, speed);
-        //             break;
-        //         case "f":
-        //         case "fly":
-        //         case "flight":
-        //             setSpeed(user, SpeedType.FLY, speed);
-        //             break;
-        //     }
-        // } else 
+
         if (((Player)sender).isFlying()) {
             setSpeed(user, SpeedType.FLY, speed);
         } else {
@@ -88,12 +71,6 @@ public class Speed extends AsyncCommand {
 
     @Override
     public List<String> tabComplete(CommandSender sender, String alias, String[] args) {
-        // if (args.length < 2) {
-        //     return Arrays.asList(new String[] {
-        //         "fly",
-        //         "walk"
-        //     });
-        // }
         if (args.length < 2) {
             return Arrays.asList(new String[] {
                 "1",
@@ -111,30 +88,16 @@ public class Speed extends AsyncCommand {
         return null;
     }
 
-    // Look, I know the code below is pretty gnarly, but believe me it's 100x better than my solution before, and the code below is actually to ensure bukkit doesn't complain
-    // so in a way this is very good to do.
-    // To explain the... mess...
-    // I need to check if the speed value is greater than 1, but also less than 0.1, if it's greater/less then we need to set a default value
-    // However! The walk speed is fucked in bukkit, so 0.1 is actually slower than the default, so we have to check if the argument is 0.1 and change it to 0.2
-    // You may ask, why use a ternary statement? Because I think it looks better, and I happen to understand them. I formatted them so whoever reads this can hopefully understand them
+
     void setSpeed(User user, SpeedType type, Float speed) {
-        switch(type) {
-            case WALK:
-                user.setSpeed(SpeedType.WALK, speed < 1.9F ? 
-                 (speed == 0.1F ? 
-                    0.2F 
-                    : (speed > 0.1F ? 
-                        speed 
-                        : 0.1F)) 
-                 : 1F);
-                break;
-            case FLY:
-                user.setSpeed(SpeedType.FLY, speed < 1.9F ? 
-                (speed > 0F ? 
-                    speed 
-                    : 0.1F) 
-                : 1F);
-                break;
-        }
+        user.setSpeed(type, speed < 1.9F 
+        ? (speed > 0F
+            ? (type == SpeedType.FLY
+                    ? speed
+                    : speed + 0.1F > 1F
+                        ? speed
+                        : speed + 0.1F)
+            : 0.1F) 
+        : 1F);
     }
 }
