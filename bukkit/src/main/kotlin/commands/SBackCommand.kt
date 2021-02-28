565a2d669e47a383b469ddbc6d527ec3bc84380d
Zachery <zachery@foxboy.sh>
2021-02-27 22:02:41 -0600

    Allow non-players to execute /sback other

--- bukkit/src/main/kotlin/commands/SBackCommand.kt
+++ bukkit/src/main/kotlin/commands/SBackCommand.kt
@@ -5,8 +5,10 @@
 package com.dumbdogdiner.stickycommands.commands
 
 import com.dumbdogdiner.stickyapi.bukkit.util.SoundUtil
+import com.dumbdogdiner.stickyapi.common.util.NotificationType
 import com.dumbdogdiner.stickycommands.util.Constants
 import dev.jorel.commandapi.arguments.PlayerArgument
+import dev.jorel.commandapi.executors.CommandExecutor
 import dev.jorel.commandapi.executors.PlayerCommandExecutor
 import org.bukkit.entity.Player
 
@@ -32,19 +34,19 @@ private val sBackSingle = commandStub("sback", Constants.Permissions.SBACK)
 
 private val sBackOther = commandStub("sback", Constants.Permissions.SBACK_OTHER)
     .withArguments(PlayerArgument("player"))
-    .executesPlayer(PlayerCommandExecutor { player, args ->
+    .executes(CommandExecutor { player, args ->
         val target = args[0] as Player
         val location = postgresHandler.getLocation(target.uniqueId)
-        val variables = playerVariables(player)
-        variables.putAll(playerVariables(target, true))
+        val variables = playerVariables(target, true)
 
         if (location == null) {
             player.sendMessage(locale.translate(Constants.LanguagePaths.BACK_NO_PREVIOUS, variables))
-            return@PlayerCommandExecutor SoundUtil.sendError(player)
+            SoundUtil.send(player, NotificationType.ERROR)
+            return@CommandExecutor
         }
 
         target.teleport(location)
         target.sendMessage(locale.translate(Constants.LanguagePaths.BACK_TELEPORTED, variables))
         player.sendMessage(locale.translate(Constants.LanguagePaths.BACK_TELEPORTED_OTHER, variables))
-        return@PlayerCommandExecutor SoundUtil.sendSuccess(player)
+        SoundUtil.send(player, NotificationType.SUCCESS)
     })
